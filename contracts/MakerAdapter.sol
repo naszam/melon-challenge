8a61d63ea7a2509a2133ba7dafe0b7433b9a2343
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-06-22 19:43:20 +0100

    update MakerAdapter.sol

--- contracts/MakerAdapter.sol
+++ contracts/MakerAdapter.sol
@@ -35,18 +35,20 @@ contract MakerAdapter is IAdapter, IntegrationSignatures {
     {
         // 2. Complete this function, which is meant to parse the expected assets that each function method will use
         if (_methodSelector == BORROW_SELECTOR) {
-          (address vault, uint amount) = __decodeBorrowArgs(_encodedArgs);
+          (address[] memory assets, uint[] memory amounts) = __decodeBorrowArgs(_encodedArgs);
 
-          outgoingAssets_ = new address[](1);
-          outgoingAssets_[0] = vault;
-          outgoingAmounts_[0] = amount;
+          for (uint i = 0; i < assets.length; i++) {
+              outgoingAssets_[i] = assets[i];
+              outgoingAmounts_[i] = amounts[i];
+          }
 
         }
         else if (_methodSelector == REDEEM_SELECTOR) {
-          (address usr, uint wad) = __decodeRedeemArgs(_encodedArgs);
-          outgoingAssets_ = new address[](1);
-          outgoingAssets_[0] = usr;
-          outgoingAmounts_[0] = wad;
+          (address[] memory assets, uint[] memory amounts) = __decodeRedeemArgs(_encodedArgs);
+          for (uint i = 0; i < assets.length; i++) {
+              outgoingAssets_[i] = assets[i];
+              outgoingAmounts_[i] = amounts[i];
+          }
         }
         else {
           revert("parseOutgoingAssets: _methodSelector invalid");
@@ -58,40 +60,56 @@ contract MakerAdapter is IAdapter, IntegrationSignatures {
 
 
     function borrow(bytes memory _encodedArgs)
-        internal
+        public
+        returns (address[] memory)
         {
-          (address vault, uint amount) = __decodeBorrowArgs(_encodedArgs);
-          // Convert WETH to ETH
-          IWeth(WETH).withdraw(amount);
-          IETHJoin(ETHJoin).join{value: amount}(vault);
+          (address[] memory assets, uint[] memory amounts) = __decodeBorrowArgs(_encodedArgs);
+
+          address[] memory fillAssets = new address[](assets.length);
+
+          for (uint i = 0; i < assets.length; i++) {
+            fillAssets[i] = assets[i];
+            // Convert WETH to ETH
+            IWeth(WETH).withdraw(amounts[i]);
+            IETHJoin(ETHJoin).join{value: amounts[i]}(fillAssets[i]);
+          }
+
+          return (fillAssets);
 
         }
 
     function redeem(bytes memory _encodedArgs)
-        internal
+        public
+        returns (address[] memory)
         {
+          (address[] memory assets, uint[] memory amounts) = __decodeRedeemArgs(_encodedArgs);
 
-          (address usr, uint wad) = __decodeRedeemArgs(_encodedArgs);
-          IETHJoin(ETHJoin).exit(usr, wad);
-          // Convert ETH to WETH
-          IWeth(WETH).deposit{value: wad}();
+          address[] memory fillAssets = new address[](assets.length);
 
+          for (uint i = 0; i < assets.length; i++) {
+            fillAssets[i] = assets[i];
+            IETHJoin(ETHJoin).exit(fillAssets[i], amounts[i]);
+            // Convert ETH to WETH
+            IWeth(WETH).deposit{value: amounts[i]}();
+          }
+
+          return (fillAssets);
         }
 
     function __decodeBorrowArgs(bytes memory _encodedArgs)
         private
         pure
-        returns (address vault_, uint amount_)
+        returns (address[] memory assets_, uint[] memory amount_)
     {
-        return abi.decode(_encodedArgs, (address,uint));
+        return abi.decode(_encodedArgs, (address[],uint[]));
     }
 
     function __decodeRedeemArgs(bytes memory _encodedArgs)
         private
         pure
-        returns (address usr_, uint wad_)
+        returns (address[] memory assets_, uint[] memory amounts_)
     {
-        return abi.decode(_encodedArgs, (address,uint));
+        return abi.decode(_encodedArgs, (address[],uint[]));
     }
 
 }
